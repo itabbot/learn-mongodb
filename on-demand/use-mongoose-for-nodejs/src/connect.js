412ac5107046256ef59375d3874972305c9a6162
挨踢方丈 <itabbot@qq.com>
2023-06-28 22:10:48 +0800

    添加注释。

--- on-demand/use-mongoose-for-nodejs/src/connect.js
+++ on-demand/use-mongoose-for-nodejs/src/connect.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+// Mongoose 让您可以立即开始使用模型，而无需等待 mongoose 建立与 MongoDB 的连接。
+// 这是因为 mongoose 在内部缓冲模型函数调用。
+//
+// 所以特别的，如果您在没有连接的情况下使用模型，Mongoose 默认情况下不会抛出任何错误。
+
 mongoose
   .connect('mongodb://127.0.0.1:27017/test')
   .then(() => {
